462aae7a8af4f64e7d262fce2fd4f30b057023eb
Patrick Bustos <saintpat97@gmail.com>
2019-03-20 18:10:34 -0700

    Created save function to minimize disk writing. To-do comments

--- epic_bot.py
+++ epic_bot.py
@@ -11,11 +11,16 @@ from epic_bot_id import epic_bot_token
 from epic_bot_id import botDict
 from epic_bot_id import botAddCmd
 from epic_bot_id import botRmCmd
+from epic_bot_id import botSaveCmd
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('$'), description='Meme bot')
 
+# Put into on_message function for unique commands. Change string to server id string object
+# Put into try except
 botDict = np.load('botDict.npy').item()
 
+# Add specific server functionality so each discord server has unique commands
+# Set new variable to server id. Convert to string object. Convert all save strings to server id string object
 @bot.event
 async def on_message(message):
     msg = message.content.split()
@@ -36,7 +41,6 @@ async def on_message(message):
 
             # save newCmd and botResponse to file and temp dictionary
             botDict[newCmd] = botResponse
-            np.save('botDict.npy', botDict)
 
         else:
             botMsg = 'Bot already has command ```' + msg[1] + '```'
@@ -45,10 +49,12 @@ async def on_message(message):
     elif message.content.startswith(botRmCmd):
         if msg[1] in botDict:
             del botDict[msg[1]]
-            np.save('botDict.npy', botDict)
         else:
             botMsg = 'Command not in dictionary ```' + msg[1] + '```'
             await bot.send_message(message.channel,botMsg.format(message))
+
+    elif message.content.startswith(botSaveCmd):
+        np.save('botDict.npy', botDict)
 
     elif message.author != bot.user:
         botMsg = 'Command does not exist'
