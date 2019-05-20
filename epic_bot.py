5a781dc8adb24b57a3aa787d7a93550afebca07b
Patrick Bustos <saintpat97@gmail.com>
2019-05-20 14:46:12 -0700

    Added remove role function and per bot instance cmd prefix. Fixed confliting cmds

--- epic_bot.py
+++ epic_bot.py
@@ -29,16 +29,16 @@ async def on_message(message):
         np.save(serverId, botDictBase)
         botDict = np.load(serverId).item()
 
-
+    global botPrefix
     msg = message.content.split()
-    msg[0] = msg[0][2:] #removes the first 2 characters './'
+    msg[0] = msg[0][len(botPrefix):] #removes the first 2 characters './'
 
     # Does not work if np.load(serverId).item() doesn't exist
     if msg[0] in botDict:
         await bot.send_message(message.channel, botDict.get(msg[0]).format(message))
 
 # Add Command
-    elif message.content.startswith(botAddCmd) and msg[1] not in botDict and message.author.top_role.permissions.manage_roles:
+    elif msg[0] == botAddCmd and msg[1] not in botDict and message.author.top_role.permissions.manage_roles:
         if (msg[1] not in botDict):
             # parse command and bot response
             newCmd = msg[1]
@@ -57,7 +57,7 @@ async def on_message(message):
             await bot.send_message(message.channel, botMsg.format(message))
 
 # Remove Command
-    elif message.content.startswith(botRmCmd) and message.author.top_role.permissions.manage_roles:
+    elif msg[0] == botRmCmd and message.author.top_role.permissions.manage_roles:
         if msg[1] in botDict:
             del botDict[msg[1]]
             np.save(serverId, botDict)
@@ -66,7 +66,7 @@ async def on_message(message):
             await bot.send_message(message.channel,botMsg.format(message))
 
 # Add Role
-    elif message.content.startswith(botAddRole):
+    elif msg[0] == botAddRole:
         target = message.mentions
         if len(target) > 1:
             botMsg = '{0.author.mention}, you can edit only 1 user\'s role'
@@ -108,8 +108,52 @@ async def on_message(message):
                 botMsg = msg[1] + ' is a superior {0.author.mention}'
                 await bot.send_message(message.channel,botMsg.format(message)) 
 
+# Remove Role
+    elif msg[0] == botRmRole:
+        target = message.mentions
+        if len(target) > 1:
+            botMsg = '{0.author.mention}, you can edit only 1 user\'s role'
+            await bot.send_message(message.channel,botMsg.format(message))
+        else:
+            roleList = msg[2:]
+            roleStr = ''
+
+            #Converts message list into a single string and removes whitespace on both sides
+            for section in roleList:
+                roleStr += section + ' '
+            roleStr.strip()
+
+            roleList = message.server.roles
+            role = message.author.top_role
+
+            # Checks if role is in the server
+            for tempRole in roleList:
+                if roleStr.strip() == str(tempRole.name.strip()):
+                    role = tempRole
+                    print(role)
+                    break
+                else:
+                    role = None
+                    print('Empty')
+
+            if role is None:
+                botMsg = 'Role does not exist {0.author.mention}'
+                await bot.send_message(message.channel,botMsg.format(message))
+
+            elif message.author.top_role >= target[0].top_role:
+                if role > message.author.top_role:
+                    botMsg = 'Cannot remove role higher than self {0.author.mention}'
+                    await bot.send_message(message.channel,botMsg.format(message))
+                else:
+                    await bot.remove_roles(target[0],role)
+                    botMsg = str(role) + ' role removed from ' + target[0].mention
+                    await bot.send_message(message.channel,botMsg.format(message))
+            elif message.author.top_role < target[0].top_role:
+                botMsg = msg[1] + ' is a superior {0.author.mention}'
+                await bot.send_message(message.channel,botMsg.format(message))
+
 # Ban User
-    elif message.content.startswith(botBanCmd):
+    elif msg[0] == botBanCmd:
         if not message.author.top_role.permissions.ban_members:
             botMsg = '{0.author.mention} does not have permission to ban others'
             await bot.send_message(message.channel,botMsg.format(message))
@@ -126,7 +170,7 @@ async def on_message(message):
                     await bot.ban(target)
 
 # Kick User
-    elif message.content.startswith(botKickCmd):
+    elif msg[0] == botKickCmd:
         if not message.author.top_role.permissions.kick_members:
             botMsg = '{0.author.mention} does not have permission to kick others'
             await bot.send_message(message.channel,botMsg.format(message))
@@ -149,5 +193,8 @@ async def on_message(message):
 @bot.event
 async def on_ready():
     print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
+    print('What is the bot prefix?')
+    global botPrefix
+    botPrefix = input()
 
 bot.run(epic_bot_token)
